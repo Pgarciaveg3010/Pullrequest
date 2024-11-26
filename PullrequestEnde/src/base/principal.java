50f9063ebd0e424408928b4bc651ec273f66bd7f
1AW3-22 <i_salazargir@fpzornotza.com>
2024-11-26 11:22:51 +0100

    ismaa

--- PullrequestEnde/src/base/principal.java
+++ PullrequestEnde/src/base/principal.java
@@ -5,6 +5,10 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("Holii");
+		}
 
 	}
 
